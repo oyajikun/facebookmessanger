02778c35d36691ab29bcee1bdfd51516e4944e1e
oyajikun <oyajikun>
2017-05-16 23:04:46 +0900

    Test Future. parallel/sequantial

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -7,6 +7,7 @@ import play.api._
 import play.api.mvc._
 
 import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Failure, Success}
 
 /**
   * This controller creates an `Action` to handle HTTP requests to the
@@ -43,8 +44,21 @@ class HomeController @Inject()(actorSystem: ActorSystem) extends Controller {
       println(s"[${Thread.currentThread().getName}]:end   ${end}")
       2
     }
+    for { // parallel
+      x <- f1
+      y <- f2
+    } yield {
+      println(x + y)
+      Ok("futures")
+    }
     val fs1 = Future.sequence(Seq(f1, f2))
-    val fs2 = Future {
+    for { // parallel
+      xs <- fs1
+    } yield {
+      println(xs.sum)
+      Ok("futures")
+    }
+    val fs1 = Future {
       for {
         i <- (1 to 2)
       } yield {
@@ -55,8 +69,14 @@ class HomeController @Inject()(actorSystem: ActorSystem) extends Controller {
         println(s"[${Thread.currentThread().getName}]:end   ${end}")
         i
       }
+    }
+    for { // sequential
+      xs <- fs1
+    } yield {
+      println(xs.sum)
+      Ok("futures")
     }*/
-    val xs = (1 to 32).map { i =>
+    val xs = (1 to 8).map { i =>
       Future {
         val start = new java.util.Date()
         println(s"[${Thread.currentThread().getName}]:start ${start}")
@@ -66,12 +86,25 @@ class HomeController @Inject()(actorSystem: ActorSystem) extends Controller {
         i
       }
     }
-    val fs3 = Future.sequence(xs)
-    for {
+    val ef1 = Future {
+      Thread.sleep(1000)
+      throw new RuntimeException("hogehoge")
+      1
+    }
+    val ys = ef1 +: xs
+    val fs3 = Future.sequence(ys)
+    // TODO: fs3の結果を受けて次のFutureを実行する実験
+    val f1 = for { // parallel
       xs <- fs3
     } yield {
       println(xs.sum)
       Ok("futures")
+    }
+    f1.recover {
+      case e => {
+        println(e)
+        BadRequest(e.getMessage)
+      }
     }
   }
 }
